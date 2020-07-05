a852ebe79c50b02a82c348bdb44a5404ed7fb0e6
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-07-05 16:35:12 +0200

    basic api for now

---
+++ supermarket/stock/api.py
@@ -0,0 +1,30 @@
+from stock.models import Product, Manufacturer , Supplier
+from rest_framework import viewsets, permissions 
+from .serializers import ProductSerializer, ManufacturerSerializer, SupplierSerializer
+
+# Product ViewSet
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ProductSerializer
+
+# Manufacturer ViewSet
+
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ManufacturerSerializer
+
+# Supplier ViewSet
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = SupplierSerializer
